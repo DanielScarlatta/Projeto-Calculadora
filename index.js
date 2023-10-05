86e12ede55c707bed76c84c7bc73642ab149853f
Daniel Dias Scarlatta <daniel.scarlatta@capitalconsig.local>
2023-10-05 09:17:21 -0300

    praticas

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ document.getElementById("clear").addEventListener("click", function() {
 
 input.addEventListener('keydown', function(ev) {
   ev.preventDefault()
-  // if(allowedKeys.includes(ev.key)) {
-  //   input.value += ev.key
-  //   return
-  // }
+    if(allowedKeys.includes(ev.key)) {
+    input.value += ev.key
+    return
+   }
   if(ev.key === 'Backspace') {
     input.value = input.value.slice(0, -1)
   }
@@ -44,6 +44,33 @@ function calculate() {
   console.log("calculado!")
 }
 
+document.getElementById("themeSwitcher").addEventListener("click", function() {
+  if(main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color"," #aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = "light"
+  }else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color"," #666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+})
+
+document.getElementById("copyToClipboard").addEventListener("click", function(ev) {
+  const button = ev.currentTarget
+  if(button.innerText === "Copy") {
+    button.innerText = "Copied!"
+    button.classList.add('success')
+    window.navigator.clipboard.writeText(resultInput.value)
+  }else {
+    button.innerText = "Copy"
+    button.classList.remove("success")
+  }
+})
 
 
 
